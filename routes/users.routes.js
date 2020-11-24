c6f6e80e3b69716ee4bf7009126c22723c3b1352
Elvira Ramírez <elvirarp92@gmail.com>
2020-11-24 13:59:14 +0100

    define users/logout and /users/isloggedin routes

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -91,4 +91,17 @@ router.post('/login', (req, res, next) => {
   })(req, res, next)
 })
 
+router.post('/logout', (req, res, next) => {
+  req.logout()
+  return res.status(200).json({ message: 'Logout successful' })
+})
+
+router.post('/isloggedin', (req, res, next) => {
+  if (req.isAuthenticated()) {
+    return res.status(200).json(req.user)
+  } else {
+    res.status(403).json({ message: 'Unauthorized' })
+  }
+})
+
 module.exports = router
