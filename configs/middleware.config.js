d68edb6bdf5ad3dc5919d16497bfeb188c2e8c7f
Elvira Ramírez <elvirarp92@gmail.com>
2020-12-02 18:13:20 +0100

    Config CORS middleware

--- configs/middleware.config.js
+++ configs/middleware.config.js
@@ -1,10 +1,26 @@
+require('dotenv').config()
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
+const cors = require('cors')
 
-module.exports = app => {
-    app.use(logger('dev'))
-    app.use(bodyParser.json())
-    app.use(bodyParser.urlencoded({ extended: false }))
-    app.use(cookieParser())
+/* CORS config */
+
+const whitelist = [process.env.CLIENT_DOMAIN_DEV]
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
+}
+
+module.exports = (app) => {
+  app.use(cors(corsOptions))
+  app.use(logger('dev'))
+  app.use(bodyParser.json())
+  app.use(bodyParser.urlencoded({ extended: false }))
+  app.use(cookieParser())
 }
